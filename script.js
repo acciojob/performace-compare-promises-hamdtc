ad4d93e2596913914b266358855edcabd014cb9d
Mohamed t c <97926192+hamdtc@users.noreply.github.com>
2023-03-16 11:32:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Array of API URLs to fetch data from
 const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/1",
   "https://jsonplaceholder.typicode.com/todos/2",
@@ -11,5 +10,38 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/9",
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
+
+// Function to fetch data from a single API
+const fetchData = (url) => {
+  return fetch(url)
+    .then(response => response.json())
+    .catch(error => console.log(error));
+}
+
+// Measure time taken to fetch data using Promise.all
+const measurePromiseAll = () => {
+  const startTime = performance.now();
+  Promise.all(apiUrls.map(fetchData))
+    .then(data => {
+      const endTime = performance.now();
+      const timeTaken = (endTime - startTime).toFixed(2);
+      document.getElementById('output-all').innerHTML = timeTaken;
+    });
+}
+
+// Measure time taken to fetch data using Promise.any
+const measurePromiseAny = () => {
+  const startTime = performance.now();
+  Promise.any(apiUrls.map(fetchData))
+    .then(data => {
+      const endTime = performance.now();
+      const timeTaken = (endTime - startTime).toFixed(2);
+      document.getElementById('output-any').innerHTML = timeTaken;
+    });
+}
+
+// Call the two functions to measure the time taken
+measurePromiseAll();
+measurePromiseAny();
 
 // You can write your code here
